6352733e667c717525c9d23d9b1ef9bca5af4b4a
barnaske <augustobarnaske@hotmail.com>
2021-10-12 17:00:26 -0300

    Criados os atributos da classe PersonagemAtor e do enum TipoAtuacao

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/PersonagemAtor.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/PersonagemAtor.java
@@ -1,4 +1,19 @@
 package br.com.cwi.reset.augustobarnaske;
 
 public class PersonagemAtor {
+
+    private static int personagemIdCounter = 0;
+    private Integer personagemId;
+    private Ator ator;
+    private String nomePersonagem;
+    private String descricaoPersonagem;
+    private TipoAtuacao tipoAtuacao;
+
+    public PersonagemAtor(Ator ator, String nomePersonagem, String descricaoPersonagem, TipoAtuacao tipoAtuacao) {
+        this.personagemId = personagemIdCounter++;
+        this.ator = ator;
+        this.nomePersonagem = nomePersonagem;
+        this.descricaoPersonagem = descricaoPersonagem;
+        this.tipoAtuacao = tipoAtuacao;
+    }
 }
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/TipoAtuacao.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/TipoAtuacao.java
@@ -1,4 +1,16 @@
 package br.com.cwi.reset.augustobarnaske;
 
 public enum TipoAtuacao {
+    PRINCIPAL ("Principal"),
+    COADJUVANTE ("Coadjuvante");
+
+    private String descricao;
+
+    TipoAtuacao (String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
