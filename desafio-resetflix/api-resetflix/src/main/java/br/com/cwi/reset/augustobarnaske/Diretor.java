6fccd482e275ca106960e695180ffaaacd5f67f3
barnaske <augustobarnaske@hotmail.com>
2021-10-12 16:02:53 -0300

    Criados os atributos da classe diretor

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/Diretor.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/Diretor.java
@@ -1,4 +1,22 @@
 package br.com.cwi.reset.augustobarnaske;
 
+import java.time.LocalDate;
+
 public class Diretor {
+
+    private static int diretorIdCounter = 0;
+    // *Caso fosse usar uma aplicação multithread seria um problema imenso usar um counter,
+    // o ideal seria usar private static AtomicInteger atomicInteger = new AtomicInteger(0), o que seria threadsafe;
+    // como não entendo essa implementação irei usar o counter e corrijo caso os monitores achem coerente.
+    private Integer diretorId;
+    private String nome;
+    private LocalDate dataNascimento;
+    private Integer anoInicioAtividade;
+
+    public Diretor(String nome, LocalDate dataNascimento, Integer anoInicioAtividade) {
+        this.diretorId = diretorIdCounter++;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.anoInicioAtividade = anoInicioAtividade;
+    }
 }
