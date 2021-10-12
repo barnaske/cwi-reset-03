0be7c37a6d4ff6a7eb3c086bcc354a0cc5bbd19d
barnaske <augustobarnaske@hotmail.com>
2021-10-12 16:09:40 -0300

    Criados os atributos da classe Estudio e do Enum StatusAtividade

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/Estudio.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/Estudio.java
@@ -1,4 +1,19 @@
 package br.com.cwi.reset.augustobarnaske;
 
 public class Estudio {
+
+    private static int estudioIdCounter = 0;
+    private Integer estudioId;
+    private String nome;
+    private String descricao;
+    private Integer dataCriacao;
+    private StatusAtividade statusAtividade;
+
+    public Estudio(String nome, String descricao, Integer dataCriacao, StatusAtividade statusAtividade) {
+        this.estudioId = estudioIdCounter++;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataCriacao = dataCriacao;
+        this.statusAtividade = statusAtividade;
+    }
 }
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/StatusAtividade.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/augustobarnaske/StatusAtividade.java
@@ -1,4 +1,16 @@
 package br.com.cwi.reset.augustobarnaske;
 
 public enum StatusAtividade {
+    EM_ATIVIDADE ("Em Atividade"),
+    ENCERRADO ("Encerrado");
+
+    private String descricao;
+
+    StatusAtividade(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
 }
